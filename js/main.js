6af3b36aebcc599da72df67e8986070994ce7f9a
krystle mensah <krystle.webdev@gmail.com>
2019-07-16 09:38:13 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -7,3 +7,31 @@ function closeSlideMenu(){
   document.getElementById('side-menu').style.width = '0';
   document.getElementById('main').style.marginLeft = '0';
 }
+
+// MODEL
+// Get DOM Elements
+const modal = document.querySelector('#simple-modal');
+const modalBtn = document.querySelector('#popup-link');
+const closeBtn = document.querySelector('.close-btn-modal');
+
+// Events
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+// Open
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
